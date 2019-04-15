22bad67f54b6b681a21602d124af1b63970dddd5
sushantkulkarni44 <sushankulkarni44@gmail.com>
2019-04-15 20:17:52 +0530

    Auto-generated commit

--- src/app/filter-one.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterOnePipe } from './filter-one.pipe';
-
-describe('FilterOnePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterOnePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/filter-two.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterTwoPipe } from './filter-two.pipe';
-
-describe('FilterTwoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterTwoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
